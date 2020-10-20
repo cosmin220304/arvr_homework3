d8b9c43dbaa959ae55aea42fcf240ed71b7f30d3
Cosmin <cosmin.aftanase@yahoo.com>
2020-10-20 18:01:17 +0300

    .

---
+++ arvr_homework3/Assets/Scripts/ToggleGrab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGrab : MonoBehaviour
+{
+    public Animator anim;
+     
+    public void toggle()
+    {
+        anim.SetTrigger("IsGrabbing");
+    }  
+}
